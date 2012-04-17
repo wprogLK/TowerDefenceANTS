08d578408e144af9397d9e3c24c066c7814aff5c
Lukas Keller <lukas.keller@solnet.ch>
2012-04-17 17:34:43 +0200

    renamed interfaces
    	deleted:    TowerDefenceANTS_Prototype/bin/interfaces/ANTSDynamicObjects.class
    	deleted:    TowerDefenceANTS_Prototype/bin/interfaces/ANTSStaticObjects.class
    	modified:   TowerDefenceANTS_Prototype/bin/mvcFactories/abstracts/ANTSMVCFactoryMVCFactoryAbstract.class
    	deleted:    TowerDefenceANTS_Prototype/src/interfaces/ANTSDynamicObjects.java
    	deleted:    TowerDefenceANTS_Prototype/src/interfaces/ANTSStaticObjects.java
    	modified:   TowerDefenceANTS_Prototype/src/mvcFactories/abstracts/ANTSMVCFactoryMVCFactoryAbstract.java

--- TowerDefenceANTS_Prototype/src/mvcFactories/abstracts/ANTSMVCFactoryMVCFactoryAbstract.java
+++ TowerDefenceANTS_Prototype/src/mvcFactories/abstracts/ANTSMVCFactoryMVCFactoryAbstract.java
@@ -1,14 +1,10 @@
 package mvcFactories.abstracts;
 
-import interfaces.ANTSIController;
-import interfaces.ANTSIModel;
 import interfaces.ANTSIView;
 
 public abstract class ANTSMVCFactoryMVCFactoryAbstract 
 {
 	protected ANTSIView view;
-//	protected ANTSIController controller;
-//	protected ANTSIModel model;
 	
 	public ANTSMVCFactoryMVCFactoryAbstract()
 	{
