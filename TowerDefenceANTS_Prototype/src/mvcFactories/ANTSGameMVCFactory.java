152f2f0b3f646d1f0a6021d839746527c896e2b4
Lukas Keller <lukas.keller@solnet.ch>
2012-04-17 08:21:03 +0200

    start MVC and Factory pattern
    	new file:   TowerDefenceANTS_Prototype/bin/interfaces/ANTSIController.class
    	new file:   TowerDefenceANTS_Prototype/bin/interfaces/ANTSIDynamicObjects.class
    	new file:   TowerDefenceANTS_Prototype/bin/interfaces/ANTSIModel.class
    	new file:   TowerDefenceANTS_Prototype/bin/interfaces/ANTSIStaticObjects.class
    	new file:   TowerDefenceANTS_Prototype/bin/mvcFactories/ANTSGameMVCFactory.class
    	new file:   TowerDefenceANTS_Prototype/bin/mvcFactories/abstracts/ANTSMVCFactoryMVCFactoryAbstract.class
    	new file:   TowerDefenceANTS_Prototype/src/interfaces/ANTSIController.java
    	new file:   TowerDefenceANTS_Prototype/src/interfaces/ANTSIDynamicObjects.java
    	new file:   TowerDefenceANTS_Prototype/src/interfaces/ANTSIModel.java
    	new file:   TowerDefenceANTS_Prototype/src/interfaces/ANTSIStaticObjects.java
    	new file:   TowerDefenceANTS_Prototype/src/mvcFactories/ANTSGameMVCFactory.java
    	new file:   TowerDefenceANTS_Prototype/src/mvcFactories/abstracts/ANTSMVCFactoryMVCFactoryAbstract.java

---
+++ TowerDefenceANTS_Prototype/src/mvcFactories/ANTSGameMVCFactory.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package mvcFactories;
+
+import mvcFactories.abstracts.ANTSMVCFactoryMVCFactoryAbstract;
+
+/**
+ * @author lukas
+ *
+ */
+public class ANTSGameMVCFactory extends ANTSMVCFactoryMVCFactoryAbstract {
+
+	@Override
+	protected void createMVC() 
+	{
+		
+		
+	}
+
+}
---
+++ TowerDefenceANTS_Prototype/src/mvcFactories/abstracts/ANTSMVCFactoryMVCFactoryAbstract.java
@@ -0,0 +1,24 @@
+package mvcFactories.abstracts;
+
+import interfaces.ANTSIController;
+import interfaces.ANTSIModel;
+import interfaces.ANTSIView;
+
+public abstract class ANTSMVCFactoryMVCFactoryAbstract 
+{
+	protected ANTSIView view;
+	protected ANTSIController controller;
+	protected ANTSIModel model;
+	
+	public ANTSMVCFactoryMVCFactoryAbstract()
+	{
+		this.createMVC();
+	}
+	
+	public final ANTSIView getView()
+	{
+		return this.view;
+	}
+	
+	protected abstract void createMVC();
+}
